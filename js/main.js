db9721971606ed1b261d0e9c845f72f3f5590e35
Pjfranco14 <69051176+Pjfranco14@users.noreply.github.com>
2021-08-24 17:45:11 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/* Datos del formulario */ 
+
+    var nombre1 = document.querySelector("#inputNombre1").value;
+    var nombre2 = document.querySelector("#inputNombre2").value;
+    var apellidos = document.querySelector("#inputApellidos").value;
+    var correo = document.querySelector("#inputCorreo").value;
+    
+   
+
+/* programacion: */
+
+    function verificacionContrasenna(elEvento){
+    elEvento.preventDefault();
+
+    var contrasenna = document.querySelector("#inputContrasenna").value;
+    var confirmacionContrasenna = document.querySelector("#inputConfirmarContrasenna").value;
+    var respuesta = document.querySelector("#inputRespuesta");
+    var respuesta2 = document.querySelector("#inputRespuesta2");
+
+    var respuestaCorrecta = "La contraseña es correcta"
+    var respuestaIncorrecta = "La contraseña que escribiste no es la misma"
+
+    if(contrasenna == confirmacionContrasenna){
+        respuesta.innerHTML = respuestaCorrecta;
+        respuesta2.innerHTML = "";
+        
+    }else{
+        respuesta2.innerHTML = respuestaIncorrecta;
+        respuesta.innerHTML = "";
+
+        
+    }
+}
+
+    var boton = document.querySelector("#inputFormulario");
+    boton.addEventListener("submit", verificacionContrasenna);
+    
+    
+
